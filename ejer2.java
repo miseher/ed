0d9c22919904b4a03001c0bd0ca9178aca3dcde7
Miguel Sebastian <erjaxondo1990@hotmail.com>
2013-12-02 13:41:47 +0100

    pruebasubida

---
+++ ejer2.java
@@ -0,0 +1,19 @@
+/*
+Escribe un programa que visualice en la pantalla tu nombre completo en una línea y en la
+línea siguiente tu fecha de nacimiento. Para ello escribe una sentencia para escribir el nombre,
+otra para escribir tu primer apellido y otra para escribir tu segundo apellido. 
+ */
+package EjerciciosUD2;
+
+
+public class ejer2 {
+     public static void main(String args[]){
+       
+       System.out.println("Miguel");
+       System.out.println("Sebastian");
+       System.out.println("Hernandez");
+       
+       System.out.print("30/1/1990");
+       
+}
+}
